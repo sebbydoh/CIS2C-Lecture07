91d3602337a74effd02d6ffd7c8b520baa4bc3dd
Encinia <sebastienencinia@my.whccd.edu>
2020-02-03 20:57:11 -0800

    Completed first and second function of the lecture 07 activity

---
+++ script.js
@@ -0,0 +1,17 @@
+// Function 1
+
+const greeting = (name) => {
+    return "Hello " + name + "!";
+} 
+console.log(greeting("Sebastien"));
+
+// Function 2 
+
+const circumference = (diameter) => {
+    const pi = 3.14;
+    return pi * diameter;
+}
+console.log(circumference(8));
+
+// Function 3 
+
